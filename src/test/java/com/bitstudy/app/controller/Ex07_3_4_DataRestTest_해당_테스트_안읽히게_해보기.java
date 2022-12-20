8f434b2967869f7f9e02d13709015477eb59ce96
goodjob0412 <tjdwnsdks@naver.com>
2022-12-21 03:10:25 +0900

    #3 - Spring Data REST 테스트 제외 처리
    파일: Ex07_03_4
    이 프로젝트의 비지니스 로직으로 구현한 내용이 아니라 data rest 기능이고, 통합테스트라 무거우면 DB에도 영향을 준다.
    그래서 제외처리해서 전체 테스트 할때 실행되지 않도록 한다.

--- src/test/java/com/bitstudy/app/controller/Ex07_3_4_DataRestTest_해당_테스트_안읽히게_해보기.java
+++ src/test/java/com/bitstudy/app/controller/Ex07_3_4_DataRestTest_해당_테스트_안읽히게_해보기.java
@@ -5,14 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,12 +29,12 @@ data rest 와 관련된 AutoConfigration을 불러올수 있게끔 @SpringBootTe
 @DisplayName("Data REST - API 테스트 4")
 @Transactional
 
-public class DataRestTest {
+public class Ex07_3_4_DataRestTest_해당_테스트_안읽히게_해보기 {
 
 
     private final MockMvc mvc;
 
-    public DataRestTest(@Autowired MockMvc mvc) {
+    public Ex07_3_4_DataRestTest_해당_테스트_안읽히게_해보기(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
