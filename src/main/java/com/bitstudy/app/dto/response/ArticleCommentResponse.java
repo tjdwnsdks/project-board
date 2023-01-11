695f7624e52e2e81057ce1b6dd39f7cba58359a7
goodjob0412 <tjdwnsdks@naver.com>
2023-01-12 08:04:14 +0900

    #41 - 수정, 삭제를 할 떄 해당 사용자가 맞는지 확인하기 위한 응답 정보 추가
     인증 정보의 'userId' 와
    해당 게시글/댓글의 작성자 'userId' 가 동일한지 알기 위해 
    응답에서 'userId' 를 내려줘야 함.
    
    이를 response dto 에 반영함.

--- src/main/java/com/bitstudy/app/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/bitstudy/app/dto/response/ArticleCommentResponse.java
@@ -8,19 +8,22 @@ import java.time.LocalDateTime;
 /** 댓글 요청하는 컨트롤러에 응답으로 내보내는 전용 DTO 임*/
 
 /* Dto와 동작원리는 비슷하지만, 다른점은 정보의 일부만 들고 있거나 가공된 형태로 정보를 가지고 있다.
-* 예를들어 nickname 의 경우 댓글테이블에 있는게 아니라 user_account 테이블이 가지고 있는건데
-* 저 아래 ArticleCommentResponse from() 메서드 를 이용해서 가공한 다음 셋팅하는거임. */
+ * 예를들어 nickname 의 경우 댓글테이블에 있는게 아니라 user_account 테이블이 가지고 있는건데
+ * 저 아래 ArticleCommentResponse from() 메서드 를 이용해서 가공한 다음 셋팅하는거임. */
 
 public record ArticleCommentResponse(
         Long id,
         String content,
         LocalDateTime createdAt,
         String email,
-        String nickname
+        String nickname,
+
+        /*새로 추가 - 이거 바꾸면 아래 of 메서드에서도 바꿔야함*/
+        String userId
 ) implements Serializable {
 
-    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname) {
-        return new ArticleCommentResponse(id, content, createdAt, email, nickname);
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, /**/String userId/**/) {
+        /*추가*/  return new ArticleCommentResponse(id, content, createdAt, email, nickname, /**/userId/**/);
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto) { /* ArticleCommentDto 을 받아서*/
@@ -35,7 +38,8 @@ public record ArticleCommentResponse(
                 dto.content(),
                 dto.createdAt(),
                 dto.userAccountDto().email(),
-                nickname
+                nickname,
+                /*추가*/         /**/dto.userAccountDto().userId()/**/
         );
     }
 
